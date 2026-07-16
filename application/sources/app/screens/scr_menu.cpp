3983912fec19d2c66e3c426abe271427b9a001bf
nhhoang2661996-web <nhhoang2661996@gmail.com>
2026-07-16 21:55:07 +0700

    feat(Menu screen): handle move and select actions

--- application/sources/app/screens/scr_menu.cpp
+++ application/sources/app/screens/scr_menu.cpp
@@ -1,6 +1,21 @@
 #include "scr_menu.h"
 
+typedef enum {
+    EATING = 0,
+    CLEANING,
+    TRAINING,
+    BACK,
+    MENU_COUNT          
+} menu_item_t;
+
+static const char *menu_items[] = {
+    [EATING]   = "Eating",
+    [CLEANING] = "Cleaning",
+    [TRAINING] = "Training",
+    [BACK]     = "Back"
+};
 static void view_scr_menu();
+static uint8_t menu_index = EATING; //current index
 
 view_dynamic_t dyn_view_menu = {
 	{
@@ -17,6 +32,11 @@ view_screen_t scr_menu = {
 	.focus_item = 0,
 };
 
+void scr_menu_show_items(){
+	view_render.fillRect(48,25,6,8,BLACK);
+	view_render.setCursor(48,25);
+	view_render.print(menu_items[menu_index]);
+}
 
 void view_scr_menu() {
     view_render.clear();
@@ -33,9 +53,29 @@ void view_scr_menu() {
 		16, \
 		9, \
 		WHITE);
-	view_render.setCursor(48,32);
-	view_render.print("EATING");
+	scr_menu_show_items();
 }
+void scr_menu_select_items(){
+	switch (menu_index) {
+        case EATING:
+            
+            break;
+        case CLEANING:
+           
+            break;
+        case TRAINING:
+            
+            break;
+        case BACK:
+			menu_index = EATING;
+            SCREEN_TRAN(scr_home_handle, &scr_home);
+            break;
+        default:
+            break;
+    }
+}
+
+
 
 void scr_menu_handle(ak_msg_t *msg) {
 	switch (msg->sig) {
@@ -43,7 +83,17 @@ void scr_menu_handle(ak_msg_t *msg) {
 		APP_DBG_SIG("SCREEN_ENTRY\n");
 		BUZZER_PlaySound(BUZZER_SOUND_WELCOME);
 	} break;
-
+	case AC_DISPLAY_BUTTON_UP_PRESSED:{
+		menu_index = (menu_index == 0) ? (MENU_COUNT - 1) : (menu_index - 1);
+		scr_menu_show_items();
+	}break;
+	case AC_DISPLAY_BUTTON_DOWN_PRESSED:{
+		menu_index = (menu_index + 1) % MENU_COUNT;
+		scr_menu_show_items();
+	}break;
+	case AC_DISPLAY_BUTTON_MODE_PRESSED:{
+		scr_menu_select_items();
+	}break;
 	default:
 		break;
 	}
